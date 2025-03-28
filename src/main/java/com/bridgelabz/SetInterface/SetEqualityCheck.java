15b1dfab6bdce6f111a9698abd53a872d71e3755
ankush1038 <ankusharma1029@gmail.com>
2025-03-28 14:36:35 +0530

    [Ankush] Add: Set equality calculated

---
+++ src/main/java/com/bridgelabz/SetInterface/SetEqualityCheck.java
@@ -0,0 +1,26 @@
+package com.bridgelabz.SetInterface;
+
+import java.util.Set;
+import java.util.HashSet;
+
+
+public class SetEqualityCheck {
+    public static void main(String[] args) {
+        Set<Integer> set1 = new HashSet<>();
+        set1.add(1);
+        set1.add(2);
+        set1.add(3);
+
+        Set<Integer> set2 = new HashSet<>();
+        set2.add(3);
+        set2.add(2);
+        set2.add(1);
+
+        boolean result = areSetsEqual(set1, set2);
+        System.out.println("Are sets same? " + result);
+    }
+
+    public static <T> boolean areSetsEqual(Set<T> set1, Set<T> set2){
+        return set1.equals(set2);
+    }
+}
