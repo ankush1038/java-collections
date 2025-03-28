47754f24791c0bb18644575f5d80a965be12fc96
ankush1038 <ankusharma1029@gmail.com>
2025-03-28 20:52:03 +0530

    [Ankush] Add: Policy class created

---
+++ src/main/java/com/bridgelabz/InsurancePolicy/Policy.java
@@ -0,0 +1,56 @@
+package com.bridgelabz.InsurancePolicy;
+
+import java.util.Date;
+import java.util.Objects;
+
+public class Policy implements Comparable<Policy> {
+    private String policyNumber;
+    private String policyholderName;
+    private Date expiryDate;
+    private String coverageType;
+    private double premiumAmount;
+
+    public Policy(String policyNumber, String policyholderName, Date expiryDate, String coverageType, double premiumAmount) {
+        this.policyNumber = policyNumber;
+        this.policyholderName = policyholderName;
+        this.expiryDate = expiryDate;
+        this.coverageType = coverageType;
+        this.premiumAmount = premiumAmount;
+    }
+
+    public String getPolicyNumber() {
+        return policyNumber;
+    }
+
+    public Date getExpiryDate() {
+        return expiryDate;
+    }
+
+    public String getCoverageType() {
+        return coverageType;
+    }
+
+    @Override
+    public int compareTo(Policy other) {
+        return this.expiryDate.compareTo(other.expiryDate);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Policy policy = (Policy) obj;
+        return Objects.equals(policyNumber, policy.policyNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(policyNumber);
+    }
+
+    @Override
+    public String toString() {
+        return policyNumber + " - " + policyholderName + " - " + coverageType + " - ₹" + premiumAmount;
+    }
+}
+
