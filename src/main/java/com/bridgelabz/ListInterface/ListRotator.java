8f3c6753ea0cf4b447e793568e9e5e4155d143df
ankush1038 <ankusharma1029@gmail.com>
2025-03-28 13:55:16 +0530

    [Ankush] Add: List rotated and printed

--- src/main/java/com/bridgelabz/ListInterface/ListRotator.java
+++ src/main/java/com/bridgelabz/ListInterface/ListRotator.java
@@ -15,7 +15,7 @@ class ListRotator {
         list.addAll(temp);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args ) {
         Scanner sc = new Scanner(System.in);
 
         // Input for list
